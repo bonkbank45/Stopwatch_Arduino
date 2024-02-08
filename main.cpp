76b0c8513d134e52963af90e9241420f4f36813a
bonkBANK <130396000+bonkbank45@users.noreply.github.com>
2024-02-08 19:57:20 +0700

    format C++ code (main.cpp)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const int stopWatch_init = 0;
 const int stopWatch_run = 1;
@@ -17,14 +17,15 @@ int state;
 boolean lcdStatus = true;
 boolean previousState = 0;
 
-unsigned long startTime = 0;  // Variable to store the start time
-unsigned long elapsedTime = 0;  // Variable to store the elapsed time
+unsigned long startTime = 0;   // Variable to store the start time
+unsigned long elapsedTime = 0; // Variable to store the elapsed time
 boolean buttonRelease = true;
 boolean buttonRelease_1 = true;
 boolean arrived = false;
 void displayTime(unsigned long time);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(D3, INPUT);
   pinMode(D4, INPUT);
@@ -32,31 +33,38 @@ void setup() {
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
 
-  lcd.init();                      // initialize the lcd 
+  lcd.init(); // initialize the lcd
   lcd.backlight();
   state = stopWatch_init;
 }
 
-void loop() {
-    if(state == stopWatch_init) {
+void loop()
+{
+  if (state == stopWatch_init)
+  {
     previousState = 0;
     startTime = millis();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("=====Timer======");
-    lcd.setCursor(4,1);
+    lcd.setCursor(4, 1);
     lcd.print("0:00:000");
     digitalWrite(D5, HIGH);
-    if (digitalRead(D3) == 0 && buttonRelease == true) {
+    if (digitalRead(D3) == 0 && buttonRelease == true)
+    {
       digitalWrite(D7, HIGH);
       digitalWrite(D5, LOW);
       state = stopWatch_run;
       buttonRelease = false;
     }
-    else if (digitalRead(D4) == 0 && buttonRelease_1 == true){
-      if (lcdStatus == true){
+    else if (digitalRead(D4) == 0 && buttonRelease_1 == true)
+    {
+      if (lcdStatus == true)
+      {
         unsigned long pressTime = millis();
-        while(digitalRead(D4) == 0) {
-          if(millis() - pressTime >= 2000){
+        while (digitalRead(D4) == 0)
+        {
+          if (millis() - pressTime >= 2000)
+          {
             buttonRelease_1 = false;
             state = countDown_init;
             lcd.clear();
@@ -65,95 +73,123 @@ void loop() {
           delay(10);
         }
         state = lcd_OFF;
-      } else if (lcdStatus == false){
-          unsigned long pressTime = millis();
-          while(digitalRead(D4) == 0) {
-            if(millis() - pressTime >= 2000){
-              buttonRelease_1 = false;
-              state = countDown_init;
-              lcd.clear();
-              return;
-            }
-            delay(10);
-          }
-        state = lcd_ON;
       }
-    }
-    else if (digitalRead(D3) == 1) {
-      buttonRelease = true;
-    }
-    if (digitalRead(D4) == 1) {
-      buttonRelease_1 = true;
-    }
-  } else if (state == stopWatch_run) {
-    displayTime(millis() - startTime);
-      if (digitalRead(D3) == 0 && buttonRelease == true) {
-        digitalWrite(D7, LOW);
-        digitalWrite(D6, HIGH);
-        state = stopWatch_result;
-        elapsedTime = millis() - startTime;
-        
-        buttonRelease = false;
-      }
-      else if (digitalRead(D3) == 1) {
-        buttonRelease = true;
-      }
-  } else if (state == stopWatch_result) {
-      if (digitalRead(D3) == 0 && buttonRelease == true) {
-        startTime = millis() - elapsedTime;
+      else if (lcdStatus == false)
+      {
         unsigned long pressTime = millis();
-        while(digitalRead(D3) == 0) {
-          if (millis() - pressTime >= 1000){
-            digitalWrite(D6, LOW);
-            state = stopWatch_init;
-            buttonRelease = false;
+        while (digitalRead(D4) == 0)
+        {
+          if (millis() - pressTime >= 2000)
+          {
+            buttonRelease_1 = false;
+            state = countDown_init;
+            lcd.clear();
             return;
           }
           delay(10);
         }
-        digitalWrite(D7, HIGH);
-        digitalWrite(D6, LOW);
-        state = stopWatch_run;
-        buttonRelease = false;
+        state = lcd_ON;
       }
-      else if (digitalRead(D3) == 1) {
-        buttonRelease = true;
-      }       
+    }
+    else if (digitalRead(D3) == 1)
+    {
+      buttonRelease = true;
+    }
+    if (digitalRead(D4) == 1)
+    {
+      buttonRelease_1 = true;
+    }
   }
-  else if (state == lcd_ON){
+  else if (state == stopWatch_run)
+  {
+    displayTime(millis() - startTime);
+    if (digitalRead(D3) == 0 && buttonRelease == true)
+    {
+      digitalWrite(D7, LOW);
+      digitalWrite(D6, HIGH);
+      state = stopWatch_result;
+      elapsedTime = millis() - startTime;
+
+      buttonRelease = false;
+    }
+    else if (digitalRead(D3) == 1)
+    {
+      buttonRelease = true;
+    }
+  }
+  else if (state == stopWatch_result)
+  {
+    if (digitalRead(D3) == 0 && buttonRelease == true)
+    {
+      startTime = millis() - elapsedTime;
+      unsigned long pressTime = millis();
+      while (digitalRead(D3) == 0)
+      {
+        if (millis() - pressTime >= 1000)
+        {
+          digitalWrite(D6, LOW);
+          state = stopWatch_init;
+          buttonRelease = false;
+          return;
+        }
+        delay(10);
+      }
+      digitalWrite(D7, HIGH);
+      digitalWrite(D6, LOW);
+      state = stopWatch_run;
+      buttonRelease = false;
+    }
+    else if (digitalRead(D3) == 1)
+    {
+      buttonRelease = true;
+    }
+  }
+  else if (state == lcd_ON)
+  {
     lcd.backlight();
     lcdStatus = true;
     buttonRelease_1 = false;
-    if (digitalRead(D4) == 1 && previousState == 0) {
+    if (digitalRead(D4) == 1 && previousState == 0)
+    {
       state = stopWatch_init;
       buttonRelease_1 = true;
-    } else if (digitalRead(D4) == 1 && previousState == 1) {
+    }
+    else if (digitalRead(D4) == 1 && previousState == 1)
+    {
       state = countDown_init;
       buttonRelease_1 = true;
     }
   }
-  else if (state == lcd_OFF) {
+  else if (state == lcd_OFF)
+  {
     lcd.noBacklight();
     lcdStatus = false;
     buttonRelease_1 = false;
-    if (digitalRead(D4) == 1 && previousState == 0) {
+    if (digitalRead(D4) == 1 && previousState == 0)
+    {
       state = stopWatch_init;
       buttonRelease_1 = true;
-    } else if (digitalRead(D4) == 1 && previousState == 1) {
+    }
+    else if (digitalRead(D4) == 1 && previousState == 1)
+    {
       state = countDown_init;
       buttonRelease_1 = true;
     }
   }
-  else if(state == countDown_init){      
-    lcd.setCursor(0,0);
+  else if (state == countDown_init)
+  {
+    lcd.setCursor(0, 0);
     lcd.print("SetCountDownTime");
-    lcd.setCursor(7,1);
+    lcd.setCursor(7, 1);
     lcd.print(countDownSecond);
     previousState = 1;
-    if (digitalRead(D3) == 0 && buttonRelease == true) {
+    if (digitalRead(D3) == 0 && buttonRelease == true)
+    {
       unsigned long pressTime = millis();
-      while(digitalRead(D3) == 0) {
-        if (millis() - pressTime >= 1000){
+      while (digitalRead(D3) == 0)
+      {
+        if (millis() - pressTime >= 1000)
+        {
           pressTime = millis();
           arrived = false;
           state = countDown_run;
@@ -162,15 +198,19 @@ void loop() {
         delay(10);
       }
       // lcd.setCursor(0,0);
-      countDownSecond+=1;
-      lcd.setCursor(7,1);
+      countDownSecond += 1;
+      lcd.setCursor(7, 1);
       lcd.print(countDownSecond);
     }
-    else if (digitalRead(D4) == 0 && buttonRelease_1 == true) {
-      if (lcdStatus == true) {
+    else if (digitalRead(D4) == 0 && buttonRelease_1 == true)
+    {
+      if (lcdStatus == true)
+      {
         unsigned long pressTime = millis();
-        while(digitalRead(D4) == 0) {
-          if(millis() - pressTime >= 2000){
+        while (digitalRead(D4) == 0)
+        {
+          if (millis() - pressTime >= 2000)
+          {
             buttonRelease_1 = false;
             state = stopWatch_init;
             lcd.clear();
@@ -181,47 +221,58 @@ void loop() {
         }
         state = lcd_OFF;
       }
-      else if (lcdStatus == false) {
+      else if (lcdStatus == false)
+      {
         unsigned long pressTime = millis();
-          while(digitalRead(D4) == 0) {
-            if(millis() - pressTime >= 2000){
-              buttonRelease_1 = false;
-              state = stopWatch_init;
-              lcd.clear();
-              countDownSecond = 0;
-              return;
-            }
-            delay(10);
+        while (digitalRead(D4) == 0)
+        {
+          if (millis() - pressTime >= 2000)
+          {
+            buttonRelease_1 = false;
+            state = stopWatch_init;
+            lcd.clear();
+            countDownSecond = 0;
+            return;
           }
+          delay(10);
+        }
         state = lcd_ON;
       }
     }
-    if (digitalRead(D4) == 1) {
+    if (digitalRead(D4) == 1)
+    {
       buttonRelease_1 = true;
     }
-    if(digitalRead(D3) == 1){
+    if (digitalRead(D3) == 1)
+    {
       buttonRelease = true;
     }
-  }else if (state == countDown_run){
+  }
+  else if (state == countDown_run)
+  {
     unsigned long r = millis();
     while (buttonRelease == true)
     {
-      
-      if(digitalRead(D3) == 0 && countDownSecond > 0 && arrived == true){
-        lcd.setCursor(0,0);
+      if (digitalRead(D3) == 0 && countDownSecond > 0 && arrived == true)
+      {
+        lcd.setCursor(0, 0);
         lcd.print(" PauseCountDown ");
         buttonRelease = false;
       }
-      else if(millis() - r >= 1000){
+      else if (millis() - r >= 1000)
+      {
         r = millis();
-        if(countDownSecond > 0){
+        if (countDownSecond > 0)
+        {
           countDownSecond--;
-          lcd.setCursor(0,0);
+          lcd.setCursor(0, 0);
           lcd.print(" CountDownTime  ");
-          lcd.setCursor(7,1);
+          lcd.setCursor(7, 1);
           lcd.print(countDownSecond);
-        }else if(countDownSecond == 0){
-          lcd.setCursor(11,1);
+        }
+        else if (countDownSecond == 0)
+        {
+          lcd.setCursor(11, 1);
           lcd.print("Done");
           delay(200);
           lcd.clear();
@@ -229,40 +280,48 @@ void loop() {
           return;
         }
       }
-      else if(digitalRead(D3) == 1){
+      else if (digitalRead(D3) == 1)
+      {
         buttonRelease = true;
         arrived = true;
       }
       delay(10);
     }
-    if(digitalRead(D3) == 0 && buttonRelease == false && arrived == false){
+    if (digitalRead(D3) == 0 && buttonRelease == false && arrived == false)
+    {
       buttonRelease = true;
-    }else if (digitalRead(D3) == 1){
+    }
+    else if (digitalRead(D3) == 1)
+    {
       arrived = false;
     }
   }
 }
 
-void displayTime(unsigned long time) {
-    unsigned long milliseconds = time % 1000;  // Calculate milliseconds
-    unsigned long seconds = (time / 1000) % 60;  // Calculate seconds
-    unsigned long minutes = (time / 60000) % 60;  // Calculate minutes
+void displayTime(unsigned long time)
+{
+  unsigned long milliseconds = time % 1000;    // Calculate milliseconds
+  unsigned long seconds = (time / 1000) % 60;  // Calculate seconds
+  unsigned long minutes = (time / 60000) % 60; // Calculate minutes
 
-    lcd.setCursor(4,1);
+  lcd.setCursor(4, 1);
 
-    // Print the elapsed time in the format "min:sec:millis"
-    lcd.print(minutes);
-    lcd.print(":");
-    if (seconds < 10) {
-      lcd.print('0');  // Add leading zero for single-digit seconds
-    }
-    lcd.print(seconds);
-    lcd.print(":");
-    if (milliseconds < 100) {
-      lcd.print('0');  // Add leading zero for two-digit milliseconds
-    }
-    if (milliseconds < 10) {
-      lcd.print('0');  // Add leading zero for single-digit milliseconds
-    }
-    lcd.print(milliseconds);
+  // Print the elapsed time in the format "min:sec:millis"
+  lcd.print(minutes);
+  lcd.print(":");
+  if (seconds < 10)
+  {
+    lcd.print('0'); // Add leading zero for single-digit seconds
+  }
+  lcd.print(seconds);
+  lcd.print(":");
+  if (milliseconds < 100)
+  {
+    lcd.print('0'); // Add leading zero for two-digit milliseconds
+  }
+  if (milliseconds < 10)
+  {
+    lcd.print('0'); // Add leading zero for single-digit milliseconds
+  }
+  lcd.print(milliseconds);
 }
